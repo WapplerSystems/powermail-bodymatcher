317077dae7160c8eb3f01ce8f54f4dfdbf9de3c2
Christian Volkmann <info@chvolkmann.de>
2024-11-22 15:20:16 +0100

    feat: Add PoC for regex-based spam filtering

---
+++ ext_emconf.php
@@ -0,0 +1,17 @@
+<?php
+
+$EM_CONF["ws_powermail_bodymatcher"] = [
+    "title" => "Powermail - SpamShield BodyMatcher",
+    "description" => "",
+    "category" => "fe",
+    "author" => "WapplerSystems",
+    "author_email" => "volkmann@wappler.systems",
+    "state" => "alpha",
+    "clearCacheOnLoad" => 0,
+    "version" => "0.0.1",
+    "constraints" => [
+        "depends" => [],
+        "conflicts" => [],
+        "suggests" => [],
+    ],
+];
